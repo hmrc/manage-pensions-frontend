3f694016fd432b57dd488843bb6fe1ec0157be1f
Madhura <madhuranakate@gmail.com>
2018-09-05 15:39:07 +0100

    Using common error handler

--- app/connectors/SchemeDetailsConnector.scala
+++ app/connectors/SchemeDetailsConnector.scala
@@ -20,14 +20,14 @@ import com.google.inject.{ImplementedBy, Inject, Singleton}
 import config.FrontendAppConfig
 import models.PsaSchemeDetails
 import play.api.Logger
-import play.api.http.Status
+import play.api.http.Status.{BAD_REQUEST, OK}
 import play.api.libs.json.{JsError, JsResultException, JsSuccess, Json}
 import uk.gov.hmrc.http._
 import uk.gov.hmrc.play.bootstrap.http.HttpClient
 import utils.HttpResponseHelper
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Try}
+import scala.util.Failure
 
 @ImplementedBy(classOf[SchemeDetailsConnectorImpl])
 trait SchemeDetailsConnector {
@@ -37,56 +37,26 @@ trait SchemeDetailsConnector {
 }
 
 @Singleton
-class SchemeDetailsConnectorImpl @Inject()(http: HttpClient, config: FrontendAppConfig) extends SchemeDetailsConnector {
+class SchemeDetailsConnectorImpl @Inject()(http: HttpClient, config: FrontendAppConfig) extends
+  SchemeDetailsConnector with HttpResponseHelper {
 
   def getSchemeDetails(schemeIdType: String, idNumber: String)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[PsaSchemeDetails] = {
 
     val url = config.schemeDetailsUrl.format(schemeIdType, idNumber)
 
     http.GET[HttpResponse](url)(implicitly, hc, implicitly).map { response =>
-        require(response.status == Status.OK)
-
-        val json = Json.parse(response.body)
-        json.validate[PsaSchemeDetails] match {
-          case JsSuccess(value, _) => value
-          case JsError(errors) => throw new JsResultException(errors)
-        }
+      response.status match {
+        case OK =>
+          val json = Json.parse(response.body)
+          json.validate[PsaSchemeDetails] match {
+            case JsSuccess(value, _) => value
+            case JsError(errors) => throw new JsResultException(errors)
+          }
+        case _ => handleErrorResponse("GET", url)(response)
+      }
     } andThen {
-      logExceptions
-    } recoverWith {
-      translateExceptions()
-    }
-  }
-
-
-  private def translateExceptions(): PartialFunction[Throwable, Future[PsaSchemeDetails]] = {
-    case ex: BadRequestException
-      if ex.message.contains("INVALID_IDTYPE")
-    => Future.failed(InvalidSchemeIdTypeException())
-    case ex: BadRequestException
-      if ex.message.contains("INVALID_SRN")
-    => Future.failed(InvalidIdException())
-    case ex: BadRequestException
-    if ex.message.contains("INVALID_PSTR")
-    => Future.failed(InvalidIdException())
-    case ex: BadRequestException
-      if ex.message.contains("INVALID_CORRELATION_ID")
-    => Future.failed(InvalidCorrelationException())
-    case Upstream5xxResponse(_, Status.INTERNAL_SERVER_ERROR, _)
-    => Future.failed(InternalServerErrorException())
-    case Upstream5xxResponse(_, Status.SERVICE_UNAVAILABLE, _)
-    => Future.failed(ServiceUnavailableException())
-//    case _
-//    => handleErrorResponse("GET", config.schemeDetailsUrl)(_)
-  }
-
-    private def logExceptions: PartialFunction[Try[PsaSchemeDetails], Unit] = {
       case Failure(t: Throwable) => Logger.warn("Unable to get scheme details", t)
     }
-
+  }
 }
 
-sealed trait SchemeDetailsException extends Exception
-case class InvalidSchemeIdTypeException() extends SchemeDetailsException
-case class InvalidIdException() extends SchemeDetailsException
-case class InvalidCorrelationException() extends SchemeDetailsException
