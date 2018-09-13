33d1ca36f5c1b253332979b83b1bf097b4b77aa3
Jack Reed <jack.reed@digital.hmrc.gov.uk>
2018-09-13 16:26:39 +0100

    Added view and controller/tests, correct link redirection still to be added

---
+++ test/views/incorrectPsaDetailsViewSpec.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+import views.behaviours.ViewBehaviours
+import views.html.incorrectPsaDetails
+import org.jsoup.Jsoup
+
+class incorrectPsaDetailsViewSpec extends ViewBehaviours {
+
+  val messageKeyPrefix = "incorrectPsaDetails"
+
+  def createView = () => incorrectPsaDetails(frontendAppConfig)(fakeRequest, messages)
+
+  "IncorrectPsaDetails view" must {
+    behave like normalPage(createView, messageKeyPrefix,
+      messages(s"messages__${messageKeyPrefix}__title"),
+      "_text1",
+      "_text2",
+      "_linkText")
+
+    //  "have link to return to scheme details" in {
+    //    Jsoup.parse(createView().toString()).select("a[id=view-schemes]") must
+    //      haveLink(controllers.routes.ListSchemesController.onPageLoad.url)
+    //  }
+  }
+}
