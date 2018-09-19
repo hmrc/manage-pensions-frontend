49b35988e988c140ba50a129ae768c00702f1af8
Darryl Murphy <darryl.murphy@digital.hmrc.gov.uk>
2018-09-19 12:57:12 +0100

    Start SchemeReferenceNumber model

---
+++ app/models/SchemeReferenceNumber.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import play.api.mvc.PathBindable
+
+import scala.language.implicitConversions
+
+case class SchemeReferenceNumber(id: String)
+
+object SchemeReferenceNumber {
+
+  implicit def srnPathBindable(implicit stringBinder: PathBindable[String]): PathBindable[SchemeReferenceNumber] = new PathBindable[SchemeReferenceNumber] {
+
+    override def bind(key: String, value: String): Either[String, SchemeReferenceNumber] = {
+      stringBinder.bind(key, value) match {
+        case Right(x) => Right(SchemeReferenceNumber(x))
+        case _ => Left("SchemeReferenceNumber binding failed")
+      }
+    }
+
+    override def unbind(key: String, value: SchemeReferenceNumber): String = {
+      stringBinder.unbind(key, value.id)
+    }
+  }
+
+  implicit def schemeReferenceNumberToString(srn: SchemeReferenceNumber): String =
+    srn.id
+
+  implicit def stringToSchemeReferenceNumber(srn: String): SchemeReferenceNumber =
+    SchemeReferenceNumber(srn)
+}
