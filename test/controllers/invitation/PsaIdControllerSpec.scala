7c6a4ef3f4f589191aa479c73ae83f5a51defec5
abhijeetardale <abhijeetardale@github.com>
2018-09-21 09:10:42 +0100

    PODS-1526 : refactoring for controller unit tests

--- test/controllers/invitation/PsaIdControllerSpec.scala
+++ test/controllers/invitation/PsaIdControllerSpec.scala
@@ -17,111 +17,52 @@
 package controllers.invitation
 
 import connectors.FakeDataCacheConnector
-import controllers.ControllerSpecBase
 import controllers.actions._
+import controllers.behaviours.QuestionPageBehaviours
 import forms.invitation.PsaIdFromProvider
 import identifiers.{PSAId, PsaNameId}
 import models.NormalMode
 import play.api.data.Form
-import play.api.libs.json.Json
-import play.api.mvc.Call
 import play.api.test.FakeRequest
-import play.api.test.Helpers._
-import utils.{UserAnswers, FakeNavigator}
+import utils.UserAnswers
 import views.html.invitation.psaId
 
-class PsaIdControllerSpec extends ControllerSpecBase {
+class PsaIdControllerSpec extends QuestionPageBehaviours {
 
   val formProvider = new PsaIdFromProvider()
   val form = formProvider()
 
-  val navigator =  new FakeNavigator(onwardRoute)
-
   val userAnswer = UserAnswers().set(PsaNameId)("xyz").asOpt.value
   val userAnswerWithPsaID = userAnswer.set(PSAId)("A0000000").asOpt.value
-  val data = getDataRetrieval(userAnswer)
 
-  def onwardRoute = Call("GET", "/foo")
 
   def getDataRetrieval(userAnswer: UserAnswers) = {
     new FakeDataRetrievalAction(Some(userAnswer.json))
   }
 
-  def controller(dataRetrievalAction: DataRetrievalAction = getEmptyData) = new PsaIdController(
-    frontendAppConfig, messagesApi, FakeAuthAction(), navigator, FakeDataCacheConnector,
-    dataRetrievalAction, new DataRequiredActionImpl, formProvider)
-
   def viewAsString(form: Form[_] = form) = psaId(frontendAppConfig, form, "xyz", NormalMode)(fakeRequest, messages).toString
 
-  "PsaIdController calling onPageLoad" must {
+  def onPageLoadAction(dataRetrievalAction: DataRetrievalAction, fakeAuth: AuthAction) = {
 
-    "return OK and the correct view for a GET" in {
-
-      val result =  controller(data).onPageLoad(NormalMode)(FakeRequest())
-
-      status(result) mustBe OK
-      contentAsString(result) mustBe viewAsString()
-    }
-
-    "populate the view correctly on a GET when the question has previously been answered" in {
-
-      val data = getDataRetrieval(userAnswerWithPsaID)
-      val result =  controller(data).onPageLoad(NormalMode)(FakeRequest())
-
-      status(result) mustBe OK
-      contentAsString(result) mustBe viewAsString(form.fill("A0000000"))
-
-    }
-
-    "redirect to Session Expired if no existing data is found" in {
-
-      val result =  controller().onPageLoad(NormalMode)(FakeRequest())
-
-      status(result) mustBe SEE_OTHER
-      redirectLocation(result) mustBe Some(controllers.routes.SessionExpiredController.onPageLoad().url)
-    }
-
-    "return 303 if user action is not authenticated" in {
-
-      val controller =  new PsaIdController(
-        frontendAppConfig, messagesApi, FakeUnAuthorisedAction(), navigator, FakeDataCacheConnector,
-        getEmptyData, new DataRequiredActionImpl, formProvider)
-
-      val result =  controller.onPageLoad(NormalMode)(FakeRequest())
-
-      status(result) mustBe SEE_OTHER
-      redirectLocation(result) mustBe Some(controllers.routes.UnauthorisedController.onPageLoad.url)
-    }
-
+    new PsaIdController(
+      frontendAppConfig, messagesApi, fakeAuth, navigator, FakeDataCacheConnector,
+      dataRetrievalAction, requiredDateAction, formProvider).onPageLoad(NormalMode)
   }
 
-  "PsaIdController calling onSubmit" must {
+  def onSubmitAction(dataRetrievalAction: DataRetrievalAction, fakeAuth: AuthAction) = {
 
-    "return a Bad Request and errors when invalid data is submitted" in {
-
-      val boundForm = form.bind(Map("psaId" -> ""))
-      val result = controller(data).onSubmit(NormalMode)(fakeRequest)
-
-      status(result) mustBe BAD_REQUEST
-      contentAsString(result) mustBe viewAsString(boundForm)
-    }
-
-    "redirect to next screen of valid data is present" in {
-
-      val result = controller(data).onSubmit(NormalMode)(FakeRequest().withJsonBody(userAnswerWithPsaID.json))
-
-      status(result) mustBe SEE_OTHER
-      redirectLocation(result) mustBe Some(onwardRoute.url)
-    }
-
-    "redirect to Session Expired if no existing data is found" in {
-
-      val result =  controller().onPageLoad(NormalMode)(FakeRequest())
-
-      status(result) mustBe SEE_OTHER
-      redirectLocation(result) mustBe Some(controllers.routes.SessionExpiredController.onPageLoad().url)
-    }
-
+    new PsaIdController(
+      frontendAppConfig, messagesApi, fakeAuth, navigator, FakeDataCacheConnector,
+      dataRetrievalAction, requiredDateAction, formProvider).onSubmit(NormalMode)
   }
+
+
+  val postRequest = FakeRequest().withJsonBody(userAnswerWithPsaID.json)
+
+  behave like onPageLoadMethod(onPageLoadAction, getDataRetrieval(userAnswer), getDataRetrieval(userAnswerWithPsaID), form, form.fill("A0000000"), viewAsString)
+
+  behave like onSubmitMethod(onSubmitAction, getDataRetrieval(userAnswerWithPsaID), form.bind(Map("psaId" -> "")), viewAsString, postRequest)
+
+  behave like requiredDataMissing(onPageLoadAction, onSubmitAction, getEmptyData)
 
 }
