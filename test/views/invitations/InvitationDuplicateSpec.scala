a289e72c1f89a1072ab8c12c707905391fccee51
Ryan Thompson <ryan_thompson_96@hotmail.co.uk>
2018-09-25 14:59:14 +0100

    implmented dynamic content and modified controller

--- test/views/invitations/InvitationDuplicateSpec.scala
+++ test/views/invitations/InvitationDuplicateSpec.scala
@@ -1,27 +1,43 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package views.invitations
 
 import base.SpecBase
 import play.twirl.api.HtmlFormat
 import viewmodels.Message
 import views.behaviours.ViewBehaviours
+import views.html.invitations.invitation_duplicate
 
 class InvitationDuplicateSpec extends ViewBehaviours {
 
   import InvitationDuplicateSpec._
 
-    "Invitation Success Page" must {
+  "Invitation Success Page" must {
 
-      behave like normalPage(
-        createView(this),
-        messageKeyPrefix,
-        Message("messages__invitationDuplicate__heading", testInviteeName)
-      )
+    behave like normalPage(
+      createView(this),
+      messageKeyPrefix,
+      Message("messages__invitationDuplicate__heading", testInviteeName)
+    )
 
-      "state the scheme name" in {
-        createView(this) must haveElementWithText("schemeName", Message("messages__invitationDuplicate__schemeName", testSchemeName))
-      }
-
+    "state the scheme name" in {
+      createView(this) must haveElementWithText("schemeName", Message("messages__invitationDuplicate__schemeName", testSchemeName))
     }
+  }
 }
 
 object InvitationDuplicateSpec {
@@ -35,7 +51,6 @@ object InvitationDuplicateSpec {
     invitation_duplicate(
       base.frontendAppConfig,
       testInviteeName,
-      testSchemeName,
+      testSchemeName
     )(base.fakeRequest, base.messages)
-
 }
