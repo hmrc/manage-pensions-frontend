fed9d0cbbd9db58f1f8671f468f1561da6c42a5e
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2019-03-04 08:16:39 +0000

    PODS-2319 Add audit service and tests

---
+++ test/audit/AuditServiceSpec.scala
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package audit
+
+import org.scalatest.{AsyncFlatSpec, Inside, Matchers}
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.mvc.AnyContentAsEmpty
+import play.api.test.FakeRequest
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.audit.http.config.AuditingConfig
+import uk.gov.hmrc.play.audit.http.connector.{AuditConnector, AuditResult}
+import uk.gov.hmrc.play.audit.model.DataEvent
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ExecutionContext, Future}
+
+class AuditServiceSpec extends AsyncFlatSpec with Matchers with Inside {
+
+  import AuditServiceSpec._
+
+  "AuditServiceImpl" should "construct and send the correct event" in {
+
+    implicit val request: FakeRequest[AnyContentAsEmpty.type] = fakeRequest()
+
+    val event = TestAuditEvent("test-audit-payload")
+
+    auditService().sendEvent(event)
+
+    val sentEvent = FakeAuditConnector.lastSentEvent
+
+    inside(sentEvent) {
+      case DataEvent(auditSource, auditType, _, _, detail, _) =>
+        auditSource shouldBe appName
+        auditType shouldBe "TestAuditEvent"
+        detail should contain("payload" -> "test-audit-payload")
+    }
+
+  }
+
+}
+
+object AuditServiceSpec {
+
+  private val app = new GuiceApplicationBuilder()
+    .overrides(
+      bind[AuditConnector].toInstance(FakeAuditConnector)
+    )
+    .build()
+
+  def fakeRequest(): FakeRequest[AnyContentAsEmpty.type] = FakeRequest("", "")
+
+  def auditService(): AuditService = app.injector.instanceOf[AuditService]
+
+  def appName: String = app.configuration.underlying.getString("appName")
+
+}
+
+//noinspection ScalaDeprecation
+object FakeAuditConnector extends AuditConnector {
+
+  private var sentEvent: DataEvent = _
+
+  override def auditingConfig: AuditingConfig = AuditingConfig(None, false, "")
+
+  override def sendEvent(event: DataEvent)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[AuditResult] = {
+    sentEvent = event
+    super.sendEvent(event)
+  }
+
+  def lastSentEvent: DataEvent = sentEvent
+
+}
+
+case class TestAuditEvent(payload: String) extends AuditEvent {
+
+  override def auditType: String = "TestAuditEvent"
+
+  override def details: Map[String, String] =
+    Map(
+      "payload" -> payload
+    )
+
+}
