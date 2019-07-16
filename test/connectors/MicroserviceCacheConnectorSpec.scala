d964f3065a384d7ac01258b0f3ca9ec6d9526243
Vincent Vinnicombe <->
2019-07-16 15:27:14 +0100

    PODS-2756: Moving spec to behaviour to make generic

--- test/connectors/MicroserviceCacheConnectorSpec.scala
+++ test/connectors/MicroserviceCacheConnectorSpec.scala
@@ -16,320 +16,308 @@
 
 package connectors
 
-import com.github.tomakehurst.wiremock.client.WireMock._
-import identifiers.TypedIdentifier
-import org.scalatest.{AsyncWordSpec, MustMatchers, OptionValues}
-import play.api.http.Status
-import play.api.libs.json.Json
-import play.api.mvc.Results._
-import play.api.test.Helpers._
-import uk.gov.hmrc.crypto.PlainText
-import uk.gov.hmrc.http.{HeaderCarrier, HttpException}
-import utils.WireMockHelper
+import connectors.behaviour.ConnectorBehaviour
+import org.scalatest.{AsyncWordSpec, MustMatchers}
 
-import scala.concurrent.ExecutionContext.Implicits.global
+class MicroserviceCacheConnectorSpec extends AsyncWordSpec with MustMatchers with ConnectorBehaviour {
 
-class MicroserviceCacheConnectorSpec extends AsyncWordSpec with MustMatchers with WireMockHelper with OptionValues {
+  protected def userAnswersCacheConnectorUrl(id: String): String = s"/pensions-scheme/journey-cache/scheme/$id"
+  protected def userAnswersCacheConnectorLastUpdatedUrl(id: String) = s"/pensions-scheme/journey-cache/scheme/$id/lastUpdated"
 
-  protected object FakeIdentifier extends TypedIdentifier[String] {
-    override def toString: String = "fake-identifier"
+  protected def updateSchemeCacheConnectorUrl(id: String): String = s"/pensions-scheme/journey-cache/update-scheme/$id"
+  protected def updateSchemeCacheConnectorLastUpdatedUrl(id: String) = s"/pensions-scheme/journey-cache/update-scheme/$id/lastUpdated"
+
+  "UserAnswersCacheConnector" must {
+    behave like cacheConnector[UserAnswersCacheConnector](userAnswersCacheConnectorUrl, userAnswersCacheConnectorLastUpdatedUrl)
   }
 
-  override protected def portConfigKey: String = "microservice.services.pensions-scheme.port"
-
-  protected implicit val hc: HeaderCarrier = HeaderCarrier()
-
-  protected def url(id: String): String = s"/pensions-scheme/journey-cache/scheme/$id"
-
-  protected def lastUpdatedUrl(id: String) = s"/pensions-scheme/journey-cache/scheme/$id/lastUpdated"
-
-  protected lazy val connector: UserAnswersCacheConnector = injector.instanceOf[MicroserviceCacheConnector]
-
-
-  ".fetch" must {
-
-    "return `None` when the server returns a 404" in {
-
-      server.stubFor(
-        get(urlEqualTo(url("foo")))
-          .willReturn(
-            notFound
-          )
-      )
-
-      connector.fetch("foo") map {
-        result =>
-          result mustNot be(defined)
-      }
-    }
-
-    "return data when the server returns 200" in {
-      server.stubFor(
-        get(urlEqualTo(url("foo")))
-          .willReturn(
-            ok("{}")
-          )
-      )
-
-      connector.fetch("foo") map {
-        result =>
-          result.value mustEqual Json.obj()
-      }
-    }
-
-    "return a failed future on upstream error" in {
-
-      server.stubFor(
-        get(urlEqualTo(url("foo")))
-          .willReturn(
-            serverError
-          )
-      )
-
-      recoverToExceptionIf[HttpException] {
-        connector.fetch("foo")
-      } map {
-        _.responseCode mustEqual Status.INTERNAL_SERVER_ERROR
-      }
-
-    }
+  "UpdateSchemeCacheConnector" must {
+    behave like cacheConnector[UpdateSchemeCacheConnector](updateSchemeCacheConnectorUrl, updateSchemeCacheConnectorLastUpdatedUrl)
   }
 
-  ".save" must {
 
-    "insert when no data exists" in {
-
-      val json = Json.obj(
-        "fake-identifier" -> "foobar"
-      )
-
-      val value = Json.stringify(json)
-
-      server.stubFor(
-        get(urlEqualTo(url("foo")))
-          .willReturn(
-            notFound
-          )
-      )
-
-      server.stubFor(
-        post(urlEqualTo(url("foo")))
-          .withRequestBody(equalTo(value))
-          .willReturn(
-            ok
-          )
-      )
-
-      connector.save("foo", FakeIdentifier, "foobar") map {
-        _ mustEqual json
-      }
-    }
-
-    "add fields to existing data" in {
-
-      val json = Json.obj(
-        "foo" -> "bar"
-      )
-
-      val updatedJson = Json.obj(
-        "foo" -> "bar",
-        "fake-identifier" -> "foobar"
-      )
-
-      val value = Json.stringify(json)
-      val updatedValue = Json.stringify(updatedJson)
-
-      server.stubFor(
-        get(urlEqualTo(url("foo")))
-          .willReturn(
-            ok(value)
-          )
-      )
-
-      server.stubFor(
-        post(urlEqualTo(url("foo")))
-          .withRequestBody(equalTo(updatedValue))
-          .willReturn(
-            ok
-          )
-      )
-
-      connector.save("foo", FakeIdentifier, "foobar") map {
-        _ mustEqual updatedJson
-      }
-    }
-
-    "update existing data" in {
-
-      val json = Json.obj(
-        "fake-identifier" -> "foo"
-      )
-
-      val updatedJson = Json.obj(
-        "fake-identifier" -> "foobar"
-      )
-
-      val value = Json.stringify(json)
-      val updatedValue = Json.stringify(updatedJson)
-
-      server.stubFor(
-        get(urlEqualTo(url("foo")))
-          .willReturn(
-            ok(value)
-          )
-      )
-
-      server.stubFor(
-        post(urlEqualTo(url("foo")))
-          .withRequestBody(equalTo(updatedValue))
-          .willReturn(
-            ok
-          )
-      )
-
-      connector.save("foo", FakeIdentifier, "foobar") map {
-        _ mustEqual updatedJson
-      }
-    }
-
-    "return a failed future on upstream error" in {
-
-      val json = Json.obj(
-        "fake-identifier" -> "foo"
-      )
-
-      val updatedJson = Json.obj(
-        "fake-identifier" -> "foobar"
-      )
-
-      val value = Json.stringify(json)
-      val updatedValue = Json.stringify(updatedJson)
-
-      server.stubFor(
-        get(urlEqualTo(url("foo")))
-          .willReturn(
-            ok(value)
-          )
-      )
-
-      server.stubFor(
-        post(urlEqualTo(url("foo")))
-          .withRequestBody(equalTo(updatedValue))
-          .willReturn(
-            serverError
-          )
-      )
-
-
-      recoverToExceptionIf[HttpException] {
-        connector.save("foo", FakeIdentifier, "foobar")
-      } map {
-        _.responseCode mustEqual Status.INTERNAL_SERVER_ERROR
-      }
-
-    }
-  }
-
-  ".lastUpdated" must {
-
-    "return `None` when the server returns a 404" in {
-
-      server.stubFor(
-        get(urlEqualTo(lastUpdatedUrl("foo")))
-          .willReturn(
-            notFound
-          )
-      )
-
-      connector.lastUpdated("foo") map {
-        result =>
-          result mustNot be(defined)
-      }
-    }
-
-    "return long value when the server returns 200" in {
-
-      val json = Json.obj(
-        "lastUpdated" -> "1528107399697"
-      )
-
-      val plaintext = PlainText("{}")
-
-      server.stubFor(
-        get(urlEqualTo(lastUpdatedUrl("foo")))
-          .willReturn(
-            ok(Json.stringify(json))
-          )
-      )
-
-      connector.lastUpdated("foo") map {
-        result =>
-          result.value mustEqual json
-      }
-    }
-
-    "return a failed future on upstream error" in {
-
-      server.stubFor(
-        get(urlEqualTo(lastUpdatedUrl("foo")))
-          .willReturn(
-            serverError
-          )
-      )
-
-      recoverToExceptionIf[HttpException] {
-        connector.lastUpdated("foo")
-      } map {
-        _.responseCode mustEqual INTERNAL_SERVER_ERROR
-      }
-
-    }
-  }
-
-  ".remove" must {
-    "remove existing data" in {
-      val json = Json.obj(
-        FakeIdentifier.toString -> "fake value",
-        "other-key" -> "meh"
-      )
-
-      val updatedJson = Json.obj(
-        "other-key" -> "meh"
-      )
-
-      val value = Json.stringify(json)
-      val updatedValue = Json.stringify(updatedJson)
-
-      server.stubFor(
-        get(urlEqualTo(url("foo")))
-          .willReturn(
-            ok(value)
-          )
-      )
-
-      server.stubFor(
-        post(urlEqualTo(url("foo")))
-          .withRequestBody(equalTo(updatedValue))
-          .willReturn(
-            ok
-          )
-      )
-
-      connector.remove("foo", FakeIdentifier) map {
-        _ mustEqual updatedJson
-      }
-    }
-  }
-
-  ".removeAll" must {
-    "remove all the data" in {
-      server.stubFor(delete(urlEqualTo(url("foo"))).
-        willReturn(ok)
-      )
-
-      connector.removeAll("foo").map {
-        _ mustEqual Ok
-      }
-    }
-  }
+//  ".fetch" must {
+//
+//    "return `None` when the server returns a 404" in {
+//
+//      server.stubFor(
+//        get(urlEqualTo(url("foo")))
+//          .willReturn(
+//            notFound
+//          )
+//      )
+//
+//      connector.fetch("foo") map {
+//        result =>
+//          result mustNot be(defined)
+//      }
+//    }
+//
+//    "return data when the server returns 200" in {
+//      server.stubFor(
+//        get(urlEqualTo(url("foo")))
+//          .willReturn(
+//            ok("{}")
+//          )
+//      )
+//
+//      connector.fetch("foo") map {
+//        result =>
+//          result.value mustEqual Json.obj()
+//      }
+//    }
+//
+//    "return a failed future on upstream error" in {
+//
+//      server.stubFor(
+//        get(urlEqualTo(url("foo")))
+//          .willReturn(
+//            serverError
+//          )
+//      )
+//
+//      recoverToExceptionIf[HttpException] {
+//        connector.fetch("foo")
+//      } map {
+//        _.responseCode mustEqual Status.INTERNAL_SERVER_ERROR
+//      }
+//
+//    }
+//  }
+//
+//  ".save" must {
+//
+//    "insert when no data exists" in {
+//
+//      val json = Json.obj(
+//        "fake-identifier" -> "foobar"
+//      )
+//
+//      val value = Json.stringify(json)
+//
+//      server.stubFor(
+//        get(urlEqualTo(url("foo")))
+//          .willReturn(
+//            notFound
+//          )
+//      )
+//
+//      server.stubFor(
+//        post(urlEqualTo(url("foo")))
+//          .withRequestBody(equalTo(value))
+//          .willReturn(
+//            ok
+//          )
+//      )
+//
+//      connector.save("foo", FakeIdentifier, "foobar") map {
+//        _ mustEqual json
+//      }
+//    }
+//
+//    "add fields to existing data" in {
+//
+//      val json = Json.obj(
+//        "foo" -> "bar"
+//      )
+//
+//      val updatedJson = Json.obj(
+//        "foo" -> "bar",
+//        "fake-identifier" -> "foobar"
+//      )
+//
+//      val value = Json.stringify(json)
+//      val updatedValue = Json.stringify(updatedJson)
+//
+//      server.stubFor(
+//        get(urlEqualTo(url("foo")))
+//          .willReturn(
+//            ok(value)
+//          )
+//      )
+//
+//      server.stubFor(
+//        post(urlEqualTo(url("foo")))
+//          .withRequestBody(equalTo(updatedValue))
+//          .willReturn(
+//            ok
+//          )
+//      )
+//
+//      connector.save("foo", FakeIdentifier, "foobar") map {
+//        _ mustEqual updatedJson
+//      }
+//    }
+//
+//    "update existing data" in {
+//
+//      val json = Json.obj(
+//        "fake-identifier" -> "foo"
+//      )
+//
+//      val updatedJson = Json.obj(
+//        "fake-identifier" -> "foobar"
+//      )
+//
+//      val value = Json.stringify(json)
+//      val updatedValue = Json.stringify(updatedJson)
+//
+//      server.stubFor(
+//        get(urlEqualTo(url("foo")))
+//          .willReturn(
+//            ok(value)
+//          )
+//      )
+//
+//      server.stubFor(
+//        post(urlEqualTo(url("foo")))
+//          .withRequestBody(equalTo(updatedValue))
+//          .willReturn(
+//            ok
+//          )
+//      )
+//
+//      connector.save("foo", FakeIdentifier, "foobar") map {
+//        _ mustEqual updatedJson
+//      }
+//    }
+//
+//    "return a failed future on upstream error" in {
+//
+//      val json = Json.obj(
+//        "fake-identifier" -> "foo"
+//      )
+//
+//      val updatedJson = Json.obj(
+//        "fake-identifier" -> "foobar"
+//      )
+//
+//      val value = Json.stringify(json)
+//      val updatedValue = Json.stringify(updatedJson)
+//
+//      server.stubFor(
+//        get(urlEqualTo(url("foo")))
+//          .willReturn(
+//            ok(value)
+//          )
+//      )
+//
+//      server.stubFor(
+//        post(urlEqualTo(url("foo")))
+//          .withRequestBody(equalTo(updatedValue))
+//          .willReturn(
+//            serverError
+//          )
+//      )
+//
+//
+//      recoverToExceptionIf[HttpException] {
+//        connector.save("foo", FakeIdentifier, "foobar")
+//      } map {
+//        _.responseCode mustEqual Status.INTERNAL_SERVER_ERROR
+//      }
+//
+//    }
+//  }
+//
+//  ".lastUpdated" must {
+//
+//    "return `None` when the server returns a 404" in {
+//
+//      server.stubFor(
+//        get(urlEqualTo(lastUpdatedUrl("foo")))
+//          .willReturn(
+//            notFound
+//          )
+//      )
+//
+//      connector.lastUpdated("foo") map {
+//        result =>
+//          result mustNot be(defined)
+//      }
+//    }
+//
+//    "return long value when the server returns 200" in {
+//
+//      val json = Json.obj(
+//        "lastUpdated" -> "1528107399697"
+//      )
+//
+//      server.stubFor(
+//        get(urlEqualTo(lastUpdatedUrl("foo")))
+//          .willReturn(
+//            ok(Json.stringify(json))
+//          )
+//      )
+//
+//      connector.lastUpdated("foo") map {
+//        result =>
+//          result.value mustEqual json
+//      }
+//    }
+//
+//    "return a failed future on upstream error" in {
+//
+//      server.stubFor(
+//        get(urlEqualTo(lastUpdatedUrl("foo")))
+//          .willReturn(
+//            serverError
+//          )
+//      )
+//
+//      recoverToExceptionIf[HttpException] {
+//        connector.lastUpdated("foo")
+//      } map {
+//        _.responseCode mustEqual INTERNAL_SERVER_ERROR
+//      }
+//
+//    }
+//  }
+//
+//  ".remove" must {
+//    "remove existing data" in {
+//      val json = Json.obj(
+//        FakeIdentifier.toString -> "fake value",
+//        "other-key" -> "meh"
+//      )
+//
+//      val updatedJson = Json.obj(
+//        "other-key" -> "meh"
+//      )
+//
+//      val value = Json.stringify(json)
+//      val updatedValue = Json.stringify(updatedJson)
+//
+//      server.stubFor(
+//        get(urlEqualTo(url("foo")))
+//          .willReturn(
+//            ok(value)
+//          )
+//      )
+//
+//      server.stubFor(
+//        post(urlEqualTo(url("foo")))
+//          .withRequestBody(equalTo(updatedValue))
+//          .willReturn(
+//            ok
+//          )
+//      )
+//
+//      connector.remove("foo", FakeIdentifier) map {
+//        _ mustEqual updatedJson
+//      }
+//    }
+//  }
+//
+//  ".removeAll" must {
+//    "remove all the data" in {
+//      server.stubFor(delete(urlEqualTo(url("foo"))).
+//        willReturn(ok)
+//      )
+//
+//      connector.removeAll("foo").map {
+//        _ mustEqual Ok
+//      }
+//    }
+//  }
 }
