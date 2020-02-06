5646b30787b082cb03a75ecf06d0c7027d82d77b
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2020-02-06 16:06:31 +0000

    PODS-3889 Upgrade major versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,11 +12,11 @@ object AppDependencies {
     "uk.gov.hmrc" %% "play-health" % "3.14.0-play-26",
     "uk.gov.hmrc" %% "play-ui" % "8.3.0-play-26",
     "uk.gov.hmrc" %% "http-caching-client" % "9.0.0-play-26",
-    "uk.gov.hmrc" %% "play-conditional-form-mapping" % "0.2.0",
+    "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.2.0-play-26",
     "uk.gov.hmrc" %% "bootstrap-play-26" % "1.1.0",
     "uk.gov.hmrc" %% "play-language" % "4.2.0-play-26",
     "uk.gov.hmrc" %% "domain" % "5.6.0-play-26",
-    "uk.gov.hmrc" %% "play-whitelist-filter" % "2.0.0",
+    "uk.gov.hmrc" %% "play-whitelist-filter" % "3.1.0-play-26",
     "com.typesafe.play" %% "play-json-joda" % "2.6.10"
   )
 
