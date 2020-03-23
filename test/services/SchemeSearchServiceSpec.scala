4af6409ad428e64b0f7023184f79e064d8743162
Emma Rawsthorne <emma.rawsthorne@digital.hmrc.gov.uk>
2020-03-23 16:00:59 +0000

    PODS-61 unit tests for search service

--- test/services/SchemeSearchServiceSpec.scala
+++ test/services/SchemeSearchServiceSpec.scala
@@ -16,86 +16,123 @@
 
 package services
 
-import org.scalatestplus.mockito.MockitoSugar
 import base.SpecBase
+import connectors.admin.MinimalPsaConnector
+import connectors.scheme.ListOfSchemesConnector
+import models.{ListOfSchemes, SchemeDetail, SchemeStatus}
+import org.mockito.Matchers
+import org.mockito.Matchers.any
+import org.mockito.Mockito.when
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.concurrent.ScalaFutures
+import org.scalatestplus.mockito.MockitoSugar
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.Future
 
 class SchemeSearchServiceSpec extends SpecBase with MockitoSugar with BeforeAndAfterEach with ScalaFutures {
-  //"onSearch" when {
-  //
-  //  when(mockMinimalPsaConnector.getPsaNameFromPsaID(any())(any(), any()))
-  //    .thenReturn(Future.successful(Some(psaName)))
-  //
-  //  "return OK and the correct view when there are schemes without pagination and search on pstr" in {
-  //    when(mockSchemeSearchService.search(any(),any())(any(),any())).thenReturn(Future.successful(fullSchemes))
-  //    val pagination: Int = 10
-  //
-  //    val numberOfPages =
-  //      paginationService.divide(fullSchemes.length, pagination)
-  //
-  //    when(mockAppConfig.listSchemePagination) thenReturn pagination
-  //
-  //    val matchPSTR = "24000001IN"
-  //
-  //    val fixture = testFixture(psaIdWithSchemes)
-  //    val postRequest =
-  //      fakeRequest.withFormUrlEncodedBody(("searchText", matchPSTR))
-  //    val result = fixture.controller.onSearch(postRequest)
-  //
-  //    status(result) mustBe OK
-  //
-  //    val expectedSchemes = fullSchemes.filter(_.pstr.contains(matchPSTR))
-  //
-  //    val expected = viewAsString(
-  //      schemes = expectedSchemes,
-  //      numberOfSchemes = expectedSchemes.length,
-  //      pagination = pagination,
-  //      pageNumber = 1,
-  //      pageNumberLinks = Seq.empty,
-  //      numberOfPages = numberOfPages,
-  //      noResultsMessageKey = None,
-  //      Some(matchPSTR)
-  //    )
-  //
-  //    contentAsString(result) mustBe expected
-  //  }
-  //
-  //  "return OK and the correct view when there are schemes without pagination and search on srn" in {
-  //    val pagination: Int = 10
-  //
-  //    val numberOfPages =
-  //      paginationService.divide(fullSchemes.length, pagination)
-  //
-  //    when(mockAppConfig.listSchemePagination) thenReturn pagination
-  //
-  //    val matchSRN = "S2400000005"
-  //
-  //    val fixture = testFixture(psaIdWithSchemes)
-  //    val postRequest =
-  //      fakeRequest.withFormUrlEncodedBody(("searchText", matchSRN))
-  //    val result = fixture.controller.onSearch(postRequest)
-  //
-  //    status(result) mustBe OK
-  //
-  //    val expectedSchemes = fullSchemes.filter(_.referenceNumber == matchSRN)
-  //
-  //    val expected = viewAsString(
-  //      schemes = expectedSchemes,
-  //      numberOfSchemes = expectedSchemes.length,
-  //      pagination = pagination,
-  //      pageNumber = 1,
-  //      pageNumberLinks = Seq.empty,
-  //      numberOfPages = numberOfPages,
-  //      noResultsMessageKey = None,
-  //      Some(matchSRN)
-  //    )
-  //
-  //    contentAsString(result) mustBe expected
-  //  }
-  //
 
-  //
+  import SchemeSearchServiceSpec._
 
+  "Search" when {
 
+    when(mockMinimalPsaConnector.getPsaNameFromPsaID(any())(any(), any()))
+      .thenReturn(Future.successful(Some(psaName)))
+
+    "return correct list of scheme details with search on correct pstr" in {
+      val mockSchemesConnector = mock[ListOfSchemesConnector]
+      when(mockSchemesConnector.getListOfSchemes(Matchers.eq(psaId))(any(), any())).thenReturn(Future.successful(listOfSchemes))
+
+      val schemeSearchService = new SchemeSearchService(mockSchemesConnector)
+
+      val expectedResult = fullSchemes.filter(_.pstr contains "24000001IN")
+
+      whenReady(schemeSearchService.search(psaId, Some("24000001IN"))) { result =>
+        result mustBe expectedResult
+      }
+    }
+
+    "return correct list of scheme details with search on correct srn" in {
+      val mockSchemesConnector = mock[ListOfSchemesConnector]
+      when(mockSchemesConnector.getListOfSchemes(Matchers.eq(psaId))(any(), any())).thenReturn(Future.successful(listOfSchemes))
+
+      val schemeSearchService = new SchemeSearchService(mockSchemesConnector)
+
+      val expectedResult = fullSchemes.filter(_.referenceNumber == "S2400000005")
+
+      whenReady(schemeSearchService.search(psaId, Some("S2400000005"))) { result =>
+        result mustBe expectedResult
+      }
+    }
+
+    "return empty list for correct format pstr/srn but no match" in {
+      val mockSchemesConnector = mock[ListOfSchemesConnector]
+      val emptyList = ListOfSchemes("", "", None)
+      when(mockSchemesConnector.getListOfSchemes(Matchers.eq(psaId))(any(), any())).thenReturn(Future.successful(emptyList))
+
+      val schemeSearchService = new SchemeSearchService(mockSchemesConnector)
+
+      val expectedResult = Nil
+
+      whenReady(schemeSearchService.search(psaId, Some("S2400000016"))) { result =>
+        result mustBe expectedResult
+      }
+    }
+
+    "return empty list for incorrect format pstr/srn" in {
+      val mockSchemesConnector = mock[ListOfSchemesConnector]
+      when(mockSchemesConnector.getListOfSchemes(Matchers.eq(psaId))(any(), any())).thenReturn(Future.successful(listOfSchemes))
+
+      val schemeSearchService = new SchemeSearchService(mockSchemesConnector)
+
+      val expectedResult = Nil
+
+      whenReady(schemeSearchService.search(psaId, Some("incorrectFormat"))) { result =>
+        result mustBe expectedResult
+      }
+    }
+  }
+}
+
+object SchemeSearchServiceSpec extends SpecBase with MockitoSugar with BeforeAndAfterEach {
+
+  implicit val hc: HeaderCarrier = HeaderCarrier()
+
+  private val mockMinimalPsaConnector: MinimalPsaConnector =
+    mock[MinimalPsaConnector]
+  private val psaName: String = "Test Psa Name"
+  private val psaId: String = "psaId"
+
+  def listOfSchemes = ListOfSchemes("", "", Some(fullSchemes))
+
+  def fullSchemes: List[SchemeDetail] =
+    List(
+      SchemeDetail(
+        name = "scheme-0",
+        referenceNumber = "srn-0",
+        schemeStatus = SchemeStatus.Open.value,
+        openDate = None,
+        pstr = Some("pstr-0"),
+        relationShip = None,
+        underAppeal = None
+      ),
+      SchemeDetail(
+        name = "scheme-1",
+        referenceNumber = "srn-1",
+        schemeStatus = SchemeStatus.Deregistered.value,
+        openDate = None,
+        pstr = Some("24000001IN"),
+        relationShip = None,
+        underAppeal = None
+      ),
+      SchemeDetail(
+        name = "scheme-2",
+        referenceNumber = "S2400000005",
+        schemeStatus = SchemeStatus.Deregistered.value,
+        openDate = None,
+        pstr = Some("pstr-2"),
+        relationShip = None,
+        underAppeal = None
+      )
+    )
 }
