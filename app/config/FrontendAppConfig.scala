8745ec3475192638c0f64ea693354876dd85ec69
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2020-05-05 14:23:30 +0100

    PODS-4201 Added frontend app conf from history

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -83,11 +83,11 @@ class FrontendAppConfig @Inject()(runModeConfiguration: Configuration, environme
   lazy val aftSummaryPageUrl: String = loadConfig("urls.aftSummaryPageLink")
   lazy val aftSummaryPageNoVersionUrl: String = loadConfig("urls.aftSummaryPageNoVersionLink")
   lazy val aftReturnHistoryUrl: String = loadConfig("urls.aftReturnHistoryLink")
-  lazy val aftContinueReturnUrl: String = loadConfig("urls.aftContinueReturn")
   lazy val aftAmendUrl: String = loadConfig("urls.aftAmendLink")
   lazy val isAFTEnabled: Boolean = runModeConfiguration.underlying.getBoolean("features.aft-return-enabled")
   lazy val aftListOfVersions: String = s"${servicesConfig.baseUrl("pension-scheme-accounting-for-tax")}${runModeConfiguration.underlying.getString("urls.aftListOfVersions")}"
   lazy val aftOverviewUrl: String = s"${servicesConfig.baseUrl("pension-scheme-accounting-for-tax")}${runModeConfiguration.underlying.getString("urls.aftOverview")}"
+  lazy val isAftNonZero: String = s"${servicesConfig.baseUrl("pension-scheme-accounting-for-tax")}${runModeConfiguration.underlying.getString("urls.isAftNonZero")}"
 
   lazy val overviewApiEnablementDate: String = "2020-07-01"
   lazy val quarterStartDate: String = "2020-04-01"
