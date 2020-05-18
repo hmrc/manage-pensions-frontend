3f1feab3a3802425e23457bdc6dcf2c224798798
Suryabala Singh <22298166+suryabalasingh@users.noreply.github.com>
2020-05-18 11:02:08 +0100

    PODS-4166-Added the auth call for sign out

--- app/controllers/LogoutController.scala
+++ app/controllers/LogoutController.scala
@@ -18,6 +18,7 @@ package controllers
 
 import config.FrontendAppConfig
 import connectors.aft.AftCacheConnector
+import controllers.actions.AuthAction
 import javax.inject.Inject
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
@@ -28,10 +29,11 @@ import scala.concurrent.ExecutionContext
 class LogoutController @Inject()(
                                   appConfig: FrontendAppConfig,
                                   aftCacheConnector: AftCacheConnector,
+                                  authenticate: AuthAction,
                                   val controllerComponents: MessagesControllerComponents
                                 )(implicit ec : ExecutionContext) extends FrontendBaseController with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = Action.async {
+  def onPageLoad: Action[AnyContent] = authenticate.async {
     implicit request =>
       aftCacheConnector.removeLock.map {_ =>
         Redirect(appConfig.serviceSignOut).withNewSession
