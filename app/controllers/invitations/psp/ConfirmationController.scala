0a524cab8245663e63e95b09e458ba5cbc3504b0
Madhura <madhuranakate@gmail.com>
2020-09-28 14:22:19 +0100

    PODS-4683 Declaration and Confirmation

---
+++ app/controllers/invitations/psp/ConfirmationController.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.invitations.psp
+
+import config.FrontendAppConfig
+import connectors.UserAnswersCacheConnector
+import controllers.Retrievals
+import controllers.actions.{AuthAction, DataRequiredAction, DataRetrievalAction}
+import identifiers.SchemeNameId
+import identifiers.invitations.psp.PspNameId
+import javax.inject.Inject
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
+import views.html.invitations.psp.confirmation
+
+import scala.concurrent.ExecutionContext
+
+class ConfirmationController @Inject()(
+                                             override val messagesApi: MessagesApi,
+                                             frontendAppConfig: FrontendAppConfig,
+                                             authenticate: AuthAction,
+                                             getData: DataRetrievalAction,
+                                             requireData: DataRequiredAction,
+                                             userAnswersCacheConnector: UserAnswersCacheConnector,
+                                             val controllerComponents: MessagesControllerComponents,
+                                             view: confirmation
+                                           )(implicit val ec: ExecutionContext) extends FrontendBaseController with I18nSupport with Retrievals {
+
+  def onPageLoad: Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
+    implicit request =>
+
+      (SchemeNameId and PspNameId).retrieve.right.map {
+        case schemeName ~ pspName =>
+          userAnswersCacheConnector.removeAll(request.externalId).map { _ =>
+            Ok(view(schemeName, pspName))
+          }
+      }
+  }
+}
---
+++ test/controllers/invitations/psp/ConfirmationControllerSpec.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.invitations.psp
+
+import java.time.LocalDate
+
+import config.FrontendAppConfig
+import connectors.FakeUserAnswersCacheConnector
+import controllers.actions._
+import controllers.behaviours.ControllerWithNormalPageBehaviours
+import identifiers.invitations.SchemeNameId
+import identifiers.invitations.psp.PspNameId
+import play.api.mvc.{Action, AnyContent, Call}
+import uk.gov.hmrc.play.bootstrap.tools.Stubs.stubMessagesControllerComponents
+import utils.UserAnswers
+import views.html.invitations.psp.confirmation
+
+class ConfirmationControllerSpec extends ControllerWithNormalPageBehaviours {
+
+
+  private val testPspName = "test-psp-name"
+  private val testSchemeName = "test-scheme-name"
+
+  private lazy val continue: Call = controllers.routes.SchemesOverviewController.onPageLoad()
+
+  private val userAnswer = UserAnswers()
+    .set(SchemeNameId)(testSchemeName).asOpt.value
+    .set(PspNameId)(testPspName).asOpt.value
+    .dataRetrievalAction
+
+  private val confirmationView = injector.instanceOf[confirmation]
+
+  def viewAsString() = confirmationView(testSchemeName, testPspName)(fakeRequest, messages).toString
+
+  private def onPageLoadAction(dataRetrievalAction: DataRetrievalAction, fakeAuth: AuthAction): Action[AnyContent] = {
+
+    new ConfirmationController(
+      messagesApi, frontendAppConfig, fakeAuth, dataRetrievalAction, requiredDateAction, FakeUserAnswersCacheConnector,
+      stubMessagesControllerComponents(), confirmationView).onPageLoad()
+  }
+
+  def testExpiryDate(config: FrontendAppConfig): LocalDate = {
+    LocalDate.now().plusDays(config.invitationExpiryDays)
+  }
+
+  def redirectionCall(): Call = onwardRoute
+
+  behave like controllerWithOnPageLoadMethod(onPageLoadAction, getEmptyData, Some(userAnswer), viewAsString)
+
+  "ConfirmationController" when {
+    "on PageLoad" must {
+      "remove all the data from the cache" in {
+        onPageLoadAction(userAnswer, FakeAuthAction())(fakeRequest)
+
+        FakeUserAnswersCacheConnector.verifyAllDataRemoved()
+      }
+    }
+  }
+
+}
