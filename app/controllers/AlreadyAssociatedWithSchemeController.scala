17543fd1785886e3559bff1e3586986f30027924
Emma Rawsthorne <27211889+emrawsy@users.noreply.github.com>
2020-10-06 10:43:53 +0100

    PODS-4688 page created

---
+++ app/controllers/AlreadyAssociatedWithSchemeController.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import com.google.inject.Inject
+import config.FrontendAppConfig
+import controllers.actions._
+import identifiers.{SchemeNameId, SchemeSrnId}
+import identifiers.invitations.psp.{PspClientReferenceId, PspNameId}
+import models.SchemeReferenceNumber
+import play.api.Mode
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, Call, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
+import views.html.alreadyAssociatedWithScheme
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class AlreadyAssociatedWithSchemeController @Inject()(appConfig: FrontendAppConfig,
+                                            override val messagesApi: MessagesApi,
+                                            authenticate: AuthAction,
+                                            getData: DataRetrievalAction,
+                                            requireData: DataRequiredAction,
+                                            val controllerComponents: MessagesControllerComponents,
+                                            view: alreadyAssociatedWithScheme)(implicit val ec: ExecutionContext) extends FrontendBaseController with Retrievals with I18nSupport {
+
+
+  def onPageLoad: Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
+    implicit request =>
+      (SchemeNameId and PspNameId).retrieve.right.map {
+        case schemeName ~ pspName =>
+          Future.successful(Ok(view(pspName, schemeName)))
+      }
+  }
+}
---
+++ test/controllers/AlreadyAssociatedWithSchemeControllerSpec.scala
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.FrontendAppConfig
+import connectors.FakeUserAnswersCacheConnector
+import controllers.actions._
+import identifiers.SchemeNameId
+import identifiers.invitations.psp.PspNameId
+import play.api.test.Helpers._
+import uk.gov.hmrc.play.bootstrap.tools.Stubs.stubMessagesControllerComponents
+import utils.UserAnswers
+import views.html.alreadyAssociatedWithScheme
+
+
+class AlreadyAssociatedWithSchemeControllerSpec extends ControllerSpecBase {
+
+  private val schemeName = "Test Scheme"
+  private val userAnswer = UserAnswers()
+    .set(PspNameId)("xyz").asOpt.value
+    .set(SchemeNameId)(schemeName).asOpt.value
+  private val minimalData = new FakeDataRetrievalAction(Some(userAnswer.json))
+  private val config = injector.instanceOf[FrontendAppConfig]
+
+
+  private val view = injector.instanceOf[alreadyAssociatedWithScheme]
+
+  def controller(dataRetrievalAction: DataRetrievalAction = minimalData) = new AlreadyAssociatedWithSchemeController(
+    config,
+    messagesApi,
+    FakeAuthAction(),
+    dataRetrievalAction,
+    new DataRequiredActionImpl,
+    stubMessagesControllerComponents(),
+    view
+  )
+
+  private def viewAsString(): String = view("xyz", schemeName)(fakeRequest, messages).toString
+
+  "PspClientReferenceController" when {
+    "on a GET" must {
+
+      "return OK and the correct view" in {
+        val result = controller().onPageLoad()(fakeRequest)
+        status(result) mustBe OK
+        contentAsString(result) mustBe viewAsString()
+      }
+
+      "redirect to the session expired page if there is no psp name" in {
+        val result = controller(getEmptyData).onPageLoad()(fakeRequest)
+        redirectLocation(result).value mustBe controllers.routes.SessionExpiredController.onPageLoad().url
+      }
+
+      "redirect to the session expired page if there is no existing data" in {
+        val result = controller(dontGetAnyData).onPageLoad()(fakeRequest)
+        redirectLocation(result).value mustBe controllers.routes.SessionExpiredController.onPageLoad().url
+      }
+    }
+  }
+}
+
+
+
+
