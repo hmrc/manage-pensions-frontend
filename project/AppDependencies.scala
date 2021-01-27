3f9175c8ac5620d673894e148ae7fb3b5b475c67
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2021-01-27 10:48:28 +0000

    libraries: Updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,8 +12,8 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-ui"                       % "8.21.0-play-27",
     "uk.gov.hmrc"       %% "http-caching-client"           % "9.2.0-play-27",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.5.0-play-27",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-27"    % "3.3.0",
-    "uk.gov.hmrc"       %% "play-language"                 % "4.9.0-play-27",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-27"    % "3.4.0",
+    "uk.gov.hmrc"       %% "play-language"                 % "4.10.0-play-27",
     "uk.gov.hmrc"       %% "domain"                        % "5.10.0-play-27",
     "uk.gov.hmrc"       %% "play-whitelist-filter"         % "3.4.0-play-27",
     "com.typesafe.play" %% "play-json-joda"                % "2.6.10",
@@ -21,7 +21,7 @@ object AppDependencies {
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"            %% "hmrctest"                    % "3.10.0-play-26",
+//    "uk.gov.hmrc"            %% "hmrctest"                    % "3.10.0-play-26",
     "org.scalatest"          %% "scalatest"                   % "3.0.8",
     "org.scalatestplus.play" %% "scalatestplus-play"          % "4.0.2",
     "org.scalacheck"         %% "scalacheck"                  % "1.14.0",
