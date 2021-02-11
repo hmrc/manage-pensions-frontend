9c5b4a3a35e5662d4bfde1c331cb788ba67e244d
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2021-02-11 11:10:03 +0000

    PODS-5118 Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,8 +6,8 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "logback-json-logger"           % "4.9.0",
-    "uk.gov.hmrc"       %% "govuk-template"                % "5.61.0-play-27",
+    "uk.gov.hmrc"       %% "logback-json-logger"           % "5.1.0",
+    "uk.gov.hmrc"       %% "govuk-template"                % "5.62.0-play-27",
     "uk.gov.hmrc"       %% "play-health"                   % "3.16.0-play-27",
     "uk.gov.hmrc"       %% "play-ui"                       % "8.21.0-play-27",
     "uk.gov.hmrc"       %% "http-caching-client"           % "9.2.0-play-27",
