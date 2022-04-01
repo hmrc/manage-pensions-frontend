d2bd08a533169f1f11dc8eb3d3a2a29fa2eb6a28
jigneshkumarpatel <86969870+jigneshkumarpatel@users.noreply.github.com>
2022-04-01 12:18:40 +0100

    PODS-6752:Add unit test

---
+++ test/controllers/triagev2/FileAccountingForTaxReturnControllerSpec.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.triagev2
+
+import controllers.ControllerSpecBase
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.test.CSRFTokenHelper.addCSRFToken
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import views.html.triagev2.fileAccountingForTaxReturn
+
+class FileAccountingForTaxReturnControllerSpec extends ControllerSpecBase with ScalaFutures with MockitoSugar {
+
+  private val application = applicationBuilder().build()
+
+  private val view = injector.instanceOf[fileAccountingForTaxReturn]
+  private val psaOverviewLink = frontendAppConfig.managePensionsUrl + controllers.routes.SchemesOverviewController.onPageLoad().url
+  private val pspOverviewLink = frontendAppConfig.managePensionsUrl + controllers.psp.routes.PspDashboardController.onPageLoad().url
+
+  "FileAccountingForTaxReturnController" must {
+
+    "return OK with the view when calling on page load for PSA" in {
+      val role = "PSA"
+      val request = addCSRFToken(FakeRequest(GET, routes.FileAccountingForTaxReturnController.onPageLoad(role).url))
+      val result = route(application, request).value
+
+      status(result) mustBe OK
+      contentAsString(result) mustBe
+        view(psaOverviewLink, frontendAppConfig.tpssWelcomeUrl)(request, messages).toString
+    }
+
+    "return OK with the view when calling on page load for PSP" in {
+      val role = "PSP"
+      val request = addCSRFToken(FakeRequest(GET, routes.FileAccountingForTaxReturnController.onPageLoad(role).url))
+      val result = route(application, request).value
+
+      status(result) mustBe OK
+      contentAsString(result) mustBe
+        view(pspOverviewLink, frontendAppConfig.tpssWelcomeUrl)(request, messages).toString
+    }
+  }
+}
+
+
+
+
---
+++ test/controllers/triagev2/WhichServiceYouWantToViewControllerSpec.scala
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.triagev2
+
+import controllers.ControllerSpecBase
+import forms.triagev2.WhichServiceYouWantToViewFormProvider
+import models.triagev2.WhichServiceYouWantToView.ManagingPensionSchemes
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.inject.bind
+import play.api.inject.guice.GuiceableModule
+import play.api.mvc.Call
+import play.api.test.CSRFTokenHelper.addCSRFToken
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import utils.annotations.TriageV2
+import utils.{FakeNavigator, Navigator}
+import views.html.triagev2.whichServiceYouWantToView
+
+class WhichServiceYouWantToViewControllerSpec extends ControllerSpecBase with ScalaFutures with MockitoSugar {
+
+  private val onwardRoute = Call("GET", "/dummy")
+  private val application = applicationBuilder(Seq[GuiceableModule](bind[Navigator].
+    qualifiedWith(classOf[TriageV2]).toInstance(new FakeNavigator(onwardRoute)))).build()
+
+  private val view = injector.instanceOf[whichServiceYouWantToView]
+  private val formProvider = new WhichServiceYouWantToViewFormProvider()
+
+  "WhichServiceYouWantToViewController" must {
+
+    "return OK with the view when calling on page load" in {
+      val request = addCSRFToken(FakeRequest(GET, routes.WhichServiceYouWantToViewController.onPageLoad("PSA").url))
+      val result = route(application, request).value
+
+      status(result) mustBe OK
+      contentAsString(result) mustBe view(formProvider("PSA"), "PSA")(request, messages).toString
+    }
+
+    "return a Bad Request and errors when invalid data is submitted" in {
+      val postRequest = FakeRequest(POST, routes.WhichServiceYouWantToViewController.onSubmit("PSA").url)
+        .withFormUrlEncodedBody("value" -> "invalid value")
+      val boundForm = formProvider("PSA").bind(Map("value" -> "invalid value"))
+      val result = route(application, postRequest).value
+
+      status(result) mustBe BAD_REQUEST
+      contentAsString(result) mustBe view(boundForm, "PSA")(postRequest, messages).toString
+
+    }
+
+    "redirect to the next page for a valid request" in {
+      val postRequest = FakeRequest(POST, routes.WhichServiceYouWantToViewController.onSubmit("PSA").url)
+        .withFormUrlEncodedBody("value" -> ManagingPensionSchemes.toString)
+      val result = route(application, postRequest).value
+
+      status(result) mustBe SEE_OTHER
+      redirectLocation(result).value mustBe onwardRoute.url
+    }
+  }
+
+}
+
+
+
+
