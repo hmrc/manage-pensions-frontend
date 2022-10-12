00cf6435baaf1ca1c7162aa249a5d460804527c4
David Saunders <79348882+sqinnydave@users.noreply.github.com>
2022-10-12 15:22:13 +0100

    PODS-MPF-cleanup refactor js and css

--- app/assets/javascripts/managepensionsfrontend.js
+++ app/assets/javascripts/managepensionsfrontend.js
@@ -1,131 +1,46 @@
-$(document).ready(function() {
+// initialise GovUK lib
+GOVUKFrontend.initAll();
+HMRCFrontend.initAll();
 
-    // If there is an error summary, set focus to the summary
-    if ($('.error-summary--show').length) {
-      $('.error-summary--show').focus()
-    }
+// prevent resubmit warning
+if (window.history && window.history.replaceState && typeof window.history.replaceState === 'function') {
+    window.history.replaceState(null, null, window.location.href);
+}
 
-    if(document.querySelectorAll('select').length > 0){
-        accessibleAutocomplete({
-            element: document.getElementById('country'),
-            id: 'country',
-            source: '/manage-pension-schemes/assets/javascripts/autocomplete/location-autocomplete-graph.json'
-        })
+document.addEventListener('DOMContentLoaded', function(event) {
 
-        accessibleAutocomplete.enhanceSelectElement({
-            defaultValue: '',
-            selectElement: document.querySelector('select')
-        })
-
-        // temporary fix for IE not registering clicks on the text of the results list for the country autocomplete
-        $('body').on('mouseup', ".autocomplete__option > strong", function(e){
-            e.preventDefault(); $(this).parent().trigger('click')
-        })
-
-        // temporary fix for the autocomplete holding onto the last matching country when a user then enters an invalid or blank country
-        $('input[role="combobox"]').on('keydown', function(e){
-            if (e.which != 13 && e.which != 9) {
-                var sel = document.querySelector('.autocomplete-wrapper select')
-                sel.value = "";
+    var backLink = document.querySelector('.govuk-back-link');
+    if (backLink) {
+        // backLink.classList.remove('js-visible');
+        backLink.addEventListener('click', function (e) {
+            e.preventDefault();
+            if (window.history && window.history.back && typeof window.history.back === 'function') {
+                window.history.back();
             }
-        })
-    }
-
-    function beforePrintCall(){
-        if($('.no-details').length > 0){
-            // store current focussed element to return focus to later
-            var fe = document.activeElement;
-            // store scroll position
-            var scrollPos = window.pageYOffset;
-            $('details').not('.open').each(function(){
-                $(this).addClass('print--open');
-                $(this).find('summary').trigger('click');
-            });
-            // blur focus off current element in case original cannot take focus back
-            $(document.activeElement).blur();
-            // return focus if possible
-            $(fe).focus();
-            // return to scroll pos
-            window.scrollTo(0,scrollPos);
-        } else {
-            $('details').attr("open","open").addClass('print--open');
-        }
-        $('details.print--open').find('summary').addClass('heading-medium');
-    }
-
-    function afterPrintCall(){
-        $('details.print--open').find('summary').removeClass('heading-medium');
-        if($('.no-details').length > 0){
-            // store current focussed element to return focus to later
-            var fe = document.activeElement;
-            // store scroll position
-            var scrollPos = window.pageYOffset;
-            $('details.print--open').each(function(){
-                $(this).removeClass('print--open');
-                $(this).find('summary').trigger('click');
-            });
-            // blur focus off current element in case original cannot take focus back
-            $(document.activeElement).blur();
-            // return focus if possible
-            $(fe).focus();
-            // return to scroll pos
-            window.scrollTo(0,scrollPos);
-        } else {
-            $('details.print--open').removeAttr("open").removeClass('print--open');
-        }
-    }
-
-    //Chrome
-    if(typeof window.matchMedia != 'undefined'){
-        mediaQueryList = window.matchMedia('print');
-        mediaQueryList.addListener(function(mql) {
-            if (mql.matches) {
-                beforePrintCall();
-            };
-            if (!mql.matches) {
-                afterPrintCall();
-            };
         });
     }
 
-    //Firefox and IE (above does not work)
-    window.onbeforeprint = function(){
-        beforePrintCall();
+    var printLink = document.querySelector('.print-this-page');
+    if (printLink) {
+        printLink.addEventListener('click', function (e) {
+            window.print();
+            return false;
+        });
     }
-    window.onafterprint = function(){
-        afterPrintCall();
+
+    // handle country picker
+    var selectEl = document.querySelector('#country')
+    if( selectEl !== null ){
+        accessibleAutocomplete.enhanceSelectElement({
+            defaultValue: "",
+            selectElement: selectEl
+        })
+
+        // fix to ensure error when blank
+        document.querySelector('input[role="combobox"]').addEventListener('keydown', function(e){
+            if (e.which != 13 && e.which != 9) {
+                selectEl.value = "";
+            }
+        });
     }
 });
-
-
-function numberInputs() {
-    // =====================================================
-    // Set currency fields to number inputs on touch devices
-    // this ensures on-screen keyboards display the correct style
-    // don't do this for FF as it has issues with trailing zeroes
-    // =====================================================
-    if($('html.touchevents').length > 0 && window.navigator.userAgent.indexOf("Firefox") == -1){
-        $('[data-type="currency"] > input[type="text"], [data-type="percentage"] > input[type="text"]').each(function(){
-            $(this).attr('type', 'number');
-            $(this).attr('step', 'any');
-            $(this).attr('min', '0');
-        });
-    }
-
-    // =====================================================
-    // Disable mouse wheel and arrow keys (38,40) for number inputs to prevent mis-entry
-    // also disable commas (188) as they will silently invalidate entry on Safari 10.0.3 and IE11
-    // =====================================================
-    $("form").on("focus", "input[type=number]", function(e) {
-        $(this).on('wheel', function(e) {
-            e.preventDefault();
-        });
-    });
-    $("form").on("blur", "input[type=number]", function(e) {
-        $(this).off('wheel');
-    });
-    $("form").on("keydown", "input[type=number]", function(e) {
-        if ( e.which == 38 || e.which == 40 || e.which == 188 )
-            e.preventDefault();
-    });
-}
