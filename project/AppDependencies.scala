6e2d298f7cc141cc40f3868457bb05a783e7c46c
cemyeniceri <cem.yeniceri@digital.hmrc.gov.uk>
2022-12-13 12:18:13 +0000

    PODS-7801 dependencies and plugins are upgraded

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,6 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"                   %% "logback-json-logger"           % "5.2.0",
     "uk.gov.hmrc"                   %% "play-ui"                       % "9.11.0-play-28",
     "uk.gov.hmrc"                   %% "http-caching-client"           % "10.0.0-play-28",
     "uk.gov.hmrc"                   %% "play-conditional-form-mapping" % "1.12.0-play-28",
