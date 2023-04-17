041cd578da1d3f29cd6771a321b72b9f711c2645
PavelVjalicin <pavelvjalicin@users.noreply.github.com>
2023-04-17 10:34:02 +0100

    PODS-8011: Revert refactor commit

--- app/utils/EventReportingHelper.scala
+++ app/utils/EventReportingHelper.scala
@@ -28,14 +28,16 @@ import scala.concurrent.{ExecutionContext, Future}
 object EventReportingHelper {
 
   final case class EventReportingData(data:EventReporting, sessionStorageKey:String)
-
   def eventReportingData(srn:String, listOfSchemes: ListOfSchemes, dataNoPstr: String => EventReporting)
                         (implicit request:AuthenticatedRequest[_]): Option[EventReportingData] = {
     val pstr = listOfSchemes.schemeDetails.flatMap (_.find (_.referenceNumber.contains (srn) ) ).flatMap (_.pstr)
     (request.session.get(SessionKeys.sessionId), pstr) match {
-      case (Some(sessionId), Some(pstr)) =>
-        val erData = EventReportingData(dataNoPstr(pstr), sessionId)
-        Some(erData)
+      case (Some(sessionId), Some(pstr)) => Some(
+        EventReportingData(
+          dataNoPstr(pstr),
+          sessionId
+        )
+      )
       case _ => None
     }
   }
